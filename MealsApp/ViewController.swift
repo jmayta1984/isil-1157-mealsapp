9b1412ab799cd3d9d65e80ee5848af5415b946b7
Jorge Mayta <jmayta@pucp.edu.pe>
2023-10-26 17:47:02 -0700

    Initial Commit

---
+++ MealsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealsApp
+//
+//  Created by user247205 on 10/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
